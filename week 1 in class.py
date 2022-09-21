9844e67154eab31cdc09751b54da805bf72049ec
Nizar Almoughrabi <75922032+NizarWho@users.noreply.github.com>
2022-09-20 20:14:41 -0400

    Add files via upload

---
+++ week 1 in class.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#Task 1: Print Hello World
+print('Hello World')
+    #Task 1.1 Hello World has been passed as a string
+
+#Task 2: Assign Hello World to an Identifier- word then print using the identifier
+word = 'Hello World'
+print(word)
+    #Task 2.1 Passed word as a variable to a print funtion no variables
+    # realize how we did not use quotes for the already stored identifier
+    
+#Task 3:  Sttrings can have either single or double quotes ' or "
+#Print the same sentance using a single ' and then " 
+nizar = "Hey My name is Nizar! 😊"
+print(nizar)
+
+quotes_string = "this is made using double quotes"
+print(quotes_string)
+quotes_string = 'this is made using a single quote'
+print(quotes_string)
+
+#or u can do this for ^^
+
+quotes_string = " this is made using double quotes"
+quotes_string1 = '\n this is made using a single quote'
+print(quotes_string, quotes_string1)
+
+#Task 4: Print the following statement- Linus Torvalds once said, "Any Program is only as good as it is useful"
+linus='Linus Torvalds once said, "Any Program is only as good as it is useful"'
+print(linus)
