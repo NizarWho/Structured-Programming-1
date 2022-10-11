2395dc3e8b4b6614929cc5f354b0b8878af27667
Nizar Almoughrabi <75922032+NizarWho@users.noreply.github.com>
2022-10-11 15:15:00 -0400

    Add files via upload

---
+++ Assignment 2.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 11 13:12:09 2022
+
+@author: nza7
+"""
+
+# First List- Loop
+colors = ['yellow', 'red', 'blue', 'green', 'black']
+for x in range(len(colors)):
+    print (colors[x])
+
+# First Neat List- Loop
+colors = ['yellow', 'blue', 'white', 'black']
+clothing = ['shirt', 'shoe', 'pants', 'hat']
+y = 0
+for x in colors:
+    print(x, 'is a great color', clothing[y])
+    y+=1
+    
+#Your first list - Loop
+colors = ['yellow is a great color hat', 'red is a great colors show', 'blue is a great color dress', 'green is a great color belt', 'black is a great color coat']
+for x in range(len(colors)):
+    print (colors[x])
+   
+    
+# or...
+colors = ['yellow', 'blue', 'white', 'black']
+clothing = ['shirt', 'shoe', 'pants', 'hat']
+y = 0
+for x in range(len(colors)):
+    print (colors[x], 'is a great color for your', clothing[y])
+    y+=1
+    
+    
+#Exercise: Working List
+careers= ['Teacher', 'Dr', 'Engineer']
+for x in careers:
+    print(careers[0])
+careers.append('chef')
+print(careers)
+careers.insert(0, 'PM')
+print(careers)
+for x in range(len(careers)):
+    print (careers[x])
+    
+# Starting from empty
+list1=[]
+list1.append('PM')
+list1.append('Teacher')
+list1.append('Dr')
+list1.append('Engineer')
+list1.append('Chef')
+print(list1)
+print(list1[0], 'is a project manager- it is hefty work!')
+print(list1[4], 'is a fun career that revolves around art')
+
+# Ordered Working List
+careers= ['Teacher', 'Dr', 'Engineer']
+print(*careers)
+for i in careers:
+   print(sorted(careers)) 
+   print(careers)
+   print(sorted(careers, reverse=True))
+   print(careers)
+   careers.reverse()
+   print(careers)
+   careers.remove('Teacher')
+   careers.remove('Engineer')
+   print(careers)
+   careers.insert(1, 'Engineer')
+   print(careers)
+   careers.insert(2, 'Teacher')
+   print(careers)
+   careers.remove('Dr')
+   careers.remove('Engineer')
+   careers.insert(1, 'Engineer')
+   careers.insert(2, 'Dr')
+   print(careers)
+   break
+
+# Ordered Numbers
+num=[0,1,2,3,4]
+print(num)
+for i in num:
+    print(sorted(num)) 
+    print(num)
+    print(sorted(num, reverse=True))
+    print(num)
+    num.reverse()
+    print(num)
+    num.reverse()
+    print(num)
+    print(careers)
+    print(sorted(num))
+    print(num)
+    print(sorted(num, reverse=True))
+    print(num)
+    break
+
+
+# List Lengths
+colors = ['yellow', 'red', 'blue', 'green', 'black']
+print(*colors, '\n this list is =', len(colors),'units long')
+careers= ['Teacher', 'Dr', 'Engineer']
+print(*careers, '\n this list is =', len(careers),'units long')
