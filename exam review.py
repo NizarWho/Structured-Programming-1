f4cc55baa5e2495f14b03ec856e4b2d95cec02e7
Nizar Almoughrabi <75922032+NizarWho@users.noreply.github.com>
2022-10-27 16:23:52 -0400

    Add files via upload

---
+++ exam review.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 27 15:35:46 2022
+
+@author: nizar
+"""
+
+tuple = ('abcd', 786 , 2.23, 'john', 70.2)?
+print (list)
+
+str = 'Hello World!'
+print(str * 2)
+
+[1, 2, 3] + [4, 5, 6]
+
+choice(seq)
+
+tuple = ('abcd', '786' , '2.23', 'john', '70.2')?
+print(tuple[1:3])
+
+L = [1,2,3,4]
+L[-2]
+
+for x in range[1, 2, 3]: 
+    print(x)
+    
+    
+if x == 5:
+     x = x + 1
+else:
+     x = 8
+
+
+if x == 5:
+    x = x + 1
+if x != 5:
+    x = 8
+    
+myvar = 18
+lst = [myvar, 2, 3]
+myvar = 22
+print (lst)
+print (myvar)
+
+plane = ("Passengers", "Luggage")
+plane[1] = "Snakes"
+print (plane)
+
+wordlist= ['Jose ', 'Sue ', 'Ivan ']
+for i in wordlist:
+    print(i *2)
+
+
+
+
